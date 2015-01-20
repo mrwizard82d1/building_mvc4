802baf5fec2e2aaa48495e47b3ccf3afcb17130e
Larry Jones <mrwizard82d1@gmail.com>
2015-01-20 11:33:38 -0600

    Ambiguous request for Search action.

--- OdeToFood/OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -8,14 +8,21 @@ namespace OdeToFood.Controllers
         //
         // GET: /Cuisine/
 
-        // Adding the http verb as an attribute means this controller is 
-        // only reached for that specific verb.
-        [HttpGet]
+        // Without HTTP verbs, invoking search is ambiguous. (Note that the
+        // MVC framework finds the request ambiguous even though R#
+        // reports that the version of Search with the optional parameter
+        // is **hidden** by the parameterless version.)
         public ActionResult Search(string name = "french")
         {
             // Back to returning simple content.
             var encodedName = Server.HtmlEncode(name);
             return Content(encodedName);
         }
+
+        public ActionResult Search()
+        {
+            return Content("Search!");
+        }
+
     }
 }
